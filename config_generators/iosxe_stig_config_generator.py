010880b9f9cf8346d25f6e6d98ee8ee405c6058e
Jimmy the Pirate <43150450+jmctsm@users.noreply.github.com>
2023-06-01 08:58:07 -0500

    Added comments

--- config_generators/iosxe_stig_config_generator.py
+++ config_generators/iosxe_stig_config_generator.py
@@ -2,10 +2,24 @@
 
 # Still need to expand for creating other parts of config
 
+"""
+  Reads in the variables from the configuration vars files and
+  then creates a configuration file for that.  This is strictly IOS
+  XE.  Requires two arguments.  One for Hostname and one for domain
+  name.
+
+  Eventually would like to to either take a EXCEL, JSON, YAML, or other
+  file and create the configuration for all including the interfaces.  Currently
+  this only applies STIG settings.
+
+  Need to expand for best practices.
+"""
+
 import time
 import yaml
 import os
 import argparse
+
 
 current_time = time.strftime("%Y%m%d_%H%M%S")
 # script_run_path = f"{ os.path.expanduser('~') }/docker_ansible_stig/"
@@ -30,6 +44,8 @@ def main():
     with open(config_file_output, "w") as config_file:
         config_file.write(f"hostname { host_name }\n")
         config_file.write(f"ip domain-name { domain_name }\n")
+        # adding this in with extra spaces so that it creates the
+        # crypto key for SSH capability
         config_file.write("crypto key gen rsa mod 2048\n\n\n\n")
         for key in fix_dict.keys():
             config_file.write(f"!{ key }")
